9dc531412a85a752d570ac2d28bdeb7714fa80bf
Enesozd1 <94735890+Enesozd1@users.noreply.github.com>
2024-04-01 15:16:56 +0600

    some

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -18,12 +18,12 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
     useEffect(() =>{
-        fetch('https://eucway-apis.onrender.com/allproducts')
+        fetch('allproducts')
         .then((response)=>response.json())
         .then((data)=>setAll_Product(data))
 
         if(localStorage.getItem('auth-token')){
-            fetch('https://eucway-apis.onrender.com/getcart', {
+            fetch('getcart', {
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -41,7 +41,7 @@ const ShopContextProvider = (props) => {
     const addToCart = (itemId) => {
         setCartItems((prev) => ({...prev,[itemId]:prev[itemId] +1}));
         if(localStorage.getItem('auth-token')){
-            fetch('https://eucway-apis.onrender.com/addtocart',{
+            fetch('addtocart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -60,7 +60,7 @@ const ShopContextProvider = (props) => {
         setCartItems((prev) => ({...prev,[itemId]:prev[itemId] -1}));
         if(localStorage.getItem('auth-token')){
             if(localStorage.getItem('auth-token')){
-                fetch('https://eucway-apis.onrender.com/removefromcart',{
+                fetch('removefromcart',{
                     method:'POST',
                     headers:{
                         Accept:'application/form-data',
